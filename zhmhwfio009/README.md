4f4319b3c27a1d3499174ab74b9c39f8ab12b21b
Danielhan <kh.han@readians.com>
2021-08-20 12:05:02 +0900

    Worklist / Analytics / Overview Page
    Worklist / Analytics / Overview Page

---
+++ zhmhwfio009/README.md
@@ -0,0 +1,36 @@
+## Application Details
+|               |
+| ------------- |
+|**Generation Date and Time**<br>Thu Aug 05 2021 22:15:37 GMT+0900 (대한민국 표준시)|
+|**App Generator**<br>@sap/generator-fiori|
+|**App Generator Version**<br>1.2.5|
+|**Generation Platform**<br>Visual Studio Code|
+|**Floorplan Used**<br>Worklist|
+|**Service Type**<br>SAP System (ABAP On Premise)|
+|**Service URL**<br>http://linux.grcplace.com:8020/sap/opu/odata/sap/ZHMHB_I_SOH_CDS/
+|**Module Name**<br>zhmhwfio009|
+|**Application Title**<br>Product-Worklist|
+|**Namespace**<br>com.readians.hmh|
+|**UI5 Theme**<br>sap_fiori_3|
+|**UI5 Version**<br>Latest|
+|**Enable Telemetry**<br>True|
+|**Main Entity**<br>ZHMHB_I_SOH|
+|**Navigation Entity**<br>None|
+
+## zhmhwfio009
+
+A Fiori application-Product-Worklist
+
+### Starting the generated app
+
+-   This app has been generated using the SAP Fiori tools - App Generator, as part of the SAP Fiori tools suite.  In order to launch the generated app, simply run the following from the generated app root folder:
+
+```
+    npm start
+```
+
+#### Pre-requisites:
+
+1. Active NodeJS LTS (Long Term Support) version and associated supported NPM version.  (See https://nodejs.org)
+
+
---
+++ zhmhwfio009/webapp/ext/controller/ListReportExt.controller.js
@@ -0,0 +1,57 @@
+sap.ui.define(
+    "com.readians.hmh.zhmhwfio009.ext.controller.ListReportExt", [
+        "sap/ui/model/Filter", 
+        "sap/ui/comp/smartfilterbar/SmartFilterBar", 
+        "sap/m/ComboBox"
+    ], function (Filter, SmartFilterBar, ComboBox) {
+        "use strict";
+        return {
+            getCustomAppStateDataExtension: function (oCustomData) {
+                //the content of the custom field will be stored in the app state, so that it can be restored later, for example after a back navigation.
+                //The developer has to ensure that the content of the field is stored in the object that is passed to this method.
+                if (oCustomData) {
+                    var oCustomField1 = this.oView.byId("Cont_test01");
+                    if (oCustomField1) {
+                        oCustomData.CsFIPo_test01 = oCustomField1.getSelectedKey();
+                    }
+                }
+            },
+
+            restoreCustomAppStateDataExtension: function (oCustomData) {
+                //in order to restore the content of the custom field in the filter bar, for example after a back navigation,
+                //an object with the content is handed over to this method. Now the developer has to ensure that the content of the custom filter is set to the control
+                if (oCustomData) {
+                    if (oCustomData.CsFIPo_test01) {
+                        var oComboBox = this.oView.byId("Cont_test01");
+                        oComboBox.setSelectedKey(
+                            oCustomData.CsFIPo_test01
+                        );
+                    }
+                }
+            },
+            onBeforeRebindTableExtension: function(oEvent) {
+                var oBindingParams = oEvent.getParameter("bindingParams");
+                oBindingParams.parameters = oBindingParams.parameters || {};
+
+                var oSmartTable = oEvent.getSource();
+                var oSmartFilterBar = this.byId(oSmartTable.getSmartFilterId());
+                if (oSmartFilterBar instanceof SmartFilterBar) {
+                    var oCustomControl = oSmartFilterBar.getControlByKey("Csfikey_filter01");
+                    if (oCustomControl instanceof ComboBox) {
+                        var vCategory = oCustomControl.getSelectedKey();
+                        switch (vCategory) {
+                            // case "0" :
+                            // 	oBindingParams.filters.push(new Filter("Supplier", "EQ", "SAP"));
+                            // 	break;
+                            // case "1" :
+                            // 	oBindingParams.filters.push(new Filter("Supplier", "EQ", "OTHERS"));
+                            // 	break;
+                            default:
+                                break;
+                        }
+                    }
+                }
+            }
+        };
+    }
+);
